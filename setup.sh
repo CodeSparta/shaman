f4ea13a2c8122191ecaf4df0532ea03f7c3692b3
jon <jhultz@redhat.com>
2020-07-06 10:31:35 -0700

    control plane
    Former-commit-id: af6240c887e725d3ed2f6cdd1f9bfd2636b3dc48

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+mkdir -p $HOME/.terraform.d/plugin-cache
+cp $HOME/fences-terraform/terraform-plugins/terraform /usr/local/bin/terraform
+cp $HOME/fences-terraform/terraform-plugins/plugins/terraform-provider-aws_v2.68.0_x4 $HOME/.terraform.d/plugin-cache
+
+ln -s $HOME/fences-terraform/provider.tf $HOME/fences-terraform/control-plane
+ln -s $HOME/fences-terraform/provider.tf $HOME/fences-terraform/elb
+ln -s $HOME/fences-terraform/provider.tf $HOME/fences-terraform/Registry-node
+ln -s $HOME/fences-terraform/provider.tf $HOME/fences-terraform/Security-groups
+
+ln -s $HOME/fences-terraform/variables.tf $HOME/fences-terraform/control-plane
+ln -s $HOME/fences-terraform/variables.tf $HOME/fences-terraform/elb
+ln -s $HOME/fences-terraform/variables.tf $HOME/fences-terraform/Registry-node
+ln -s $HOME/fences-terraform/variables.tf $HOME/fences-terraform/Security-groups

2eff2532e857555420902bb2d31801e04cb5f4af
jon <jhultz@redhat.com>
2020-07-10 09:39:15 -0700

    rds
    Former-commit-id: ddefb6dd4a97526f711fa8f77fd3557d7e47f7f4

--- setup.sh
+++ setup.sh
@@ -7,8 +7,12 @@ ln -s $HOME/fences-terraform/provider.tf $HOME/fences-terraform/control-plane
 ln -s $HOME/fences-terraform/provider.tf $HOME/fences-terraform/elb
 ln -s $HOME/fences-terraform/provider.tf $HOME/fences-terraform/Registry-node
 ln -s $HOME/fences-terraform/provider.tf $HOME/fences-terraform/Security-groups
+ln -s $HOME/fences-terraform/provider.tf $HOME/fences-terraform/postgres-rds
+ln -s $HOME/fences-terraform/provider.tf $HOME/fences-terraform/rds-db-subnet
 
 ln -s $HOME/fences-terraform/variables.tf $HOME/fences-terraform/control-plane
 ln -s $HOME/fences-terraform/variables.tf $HOME/fences-terraform/elb
 ln -s $HOME/fences-terraform/variables.tf $HOME/fences-terraform/Registry-node
 ln -s $HOME/fences-terraform/variables.tf $HOME/fences-terraform/Security-groups
+ln -s $HOME/fences-terraform/variables.tf $HOME/fences-terraform/postgres-rds
+ln -s $HOME/fences-terraform/variables.tf $HOME/fences-terraform/rds-db-subnet

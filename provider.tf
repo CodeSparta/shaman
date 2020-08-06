0a52182a492f329a44f3c1b1dc70131b3d230e2f
jon <jhultz@redhat.com>
2020-08-05 18:47:54 -0700

    testing phase 1 ansible

--- provider.tf
+++ provider.tf
@@ -1,4 +1,5 @@
 provider "aws" {
-  profile = "default"
   region  = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
--- variables.tf
+++ variables.tf
@@ -4,7 +4,8 @@ variable "cluster_name" { default = "" }
 variable "cluster_domain" { default = "" }
 variable "private_vpc_cidr" {default = "" }
 variable "rhcos_ami" { default = "" }
-
+variable "aws_access_key" { default = "" }
+variable "aws_secret_key" { default = "" }
 
 variable "ec2_type" { default = "m5.xlarge" }
 variable "volume_size" { default = "120" }

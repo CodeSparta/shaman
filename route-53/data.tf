86e7cd09e982160cd368a7fa361e5b335865e4f3
jon <jhultz@redhat.com>
2020-08-05 17:01:48 -0700

    testing phase 1 ansible

--- route-53/data.tf
+++
@@ -1,7 +0,0 @@
-data "aws_vpc" "cluster_vpc" {
-  id =  var.vpc_id
-}
-
-data "aws_lb" "control_plane_int" {
-  arn =  var.control_plane_lb_int_arn
-}
--- variables.tf
+++ variables.tf
@@ -1,13 +1,16 @@
 variable "aws_region" { default = "" }
 variable "vpc_id" { default = "" }
 variable "cluster_name" { default = "" }
-variable "default_tags" { default = {} }
+variable "cluster_domain" { default = "" }
 variable "private_vpc_cidr" {default = "" }
 variable "rhcos_ami" { default = "" }
+
+
 variable "ec2_type" { default = "m5.xlarge" }
 variable "volume_size" { default = "120" }
-variable "cluster_domain" { default = "" }
+variable "default_tags" { default = {} }
 variable "master_count" { default = "3" }
+
 variable "subnet_list" {
   description = "AWS Private Subnets"
   type = list(string)
@@ -16,7 +19,4 @@ variable "subnet_list" {
 variable "ssh_public_key" {
   default = ""
 }
-variable "rds_postgres_type" { default = "db.t2.micro"}
-variable "db_size" { default = "20"}
-variable "rds_user" { default = "clusteruser"}
-variable "rds_password" { default = "changeme"}
+

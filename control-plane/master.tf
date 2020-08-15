d25973e2ff3bb7092be68c40ec2acd22233d93f0
jrock <jonny@redhat.com>
2020-08-15 15:03:29 +0000

    Removes route53 resources from modules

--- control-plane/master.tf
+++ control-plane/master.tf
@@ -31,26 +31,3 @@ resource "aws_lb_target_group_attachment" "master_22623" {
   target_group_arn = data.aws_lb_target_group.int_22623_tg.arn
   target_id        = aws_instance.master-nodes[count.index].private_ip
 }
-
-resource "aws_route53_record" "etcd_srv" {
-  allow_overwrite = "true"
-  name = "_etcd-server-ssl._tcp"
-  type = "SRV"
-  zone_id = aws_route53_zone.private_zone.zone_id
-  records = [
-    "0 10 2380 etcd-0.${var.cluster_name}.${var.cluster_domain}",
-    "0 10 2380 etcd-1.${var.cluster_name}.${var.cluster_domain}",
-    "0 10 2380 etcd-2.${var.cluster_name}.${var.cluster_domain}"
-  ]
-  ttl = 300
-}
-
-resource "aws_route53_record" "etcd_a_nodes" {
-  count = var.control_plane.count
-  type    = "A"
-  ttl     = "60"
-  allow_overwrite = true
-  zone_id = data.aws_route53_zone.zone.id
-  name    = "etcd-${count.index}.${var.cluster_name}.${var.cluster_domain}"
-  records = ["${aws_instance.master-nodes[count.index].private_ip}"]
-}

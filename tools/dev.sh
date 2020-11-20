94c80063f3c9a8b8282e0f5ee534f18608fcd815
Kathryn Morgan <kat@braincraft.io>
2020-11-19 16:53:26 -0800

    Update dev.sh

--- tools/dev.sh
+++ tools/dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 clear
 sudo podman run -it --rm \
-    --name shaman -h shaman--entrypoint bash \
+    --name shaman -h shaman --entrypoint bash \
     --volume $(pwd):/root/platform/iac/shaman:z \
     --workdir /root/platform/iac/shaman \
   docker.io/cloudctl/konductor

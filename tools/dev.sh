a416b64482bc1b0dcbad658e840e5c9993242bec
Kathryn Morgan <kat@braincraft.io>
2020-11-19 16:52:28 -0800

    Update dev.sh

--- tools/dev.sh
+++ tools/dev.sh
@@ -4,4 +4,4 @@ sudo podman run -it --rm \
     --name shaman -h shaman--entrypoint bash \
     --volume $(pwd):/root/platform/iac/shaman:z \
     --workdir /root/platform/iac/shaman \
-  docker.io/codesparta/konductor
+  quay.io/cloudctl/konductor
